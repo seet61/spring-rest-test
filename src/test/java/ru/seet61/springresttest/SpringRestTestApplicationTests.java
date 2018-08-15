cd7a8ca5ebe3f49b28cf8ba9f0b650dbc2f690c7
seet61 <seet_seet@mail.ru>
2018-08-15 09:29:07 +0300

    Initial commit

---
+++ src/test/java/ru/seet61/springresttest/SpringRestTestApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.seet61.springresttest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
